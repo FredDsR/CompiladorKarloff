fb9343f1088079ec558113fbe0eda139e6f73dc2
Frederico Reckziegel <freddalsoglio@gmail.com>
2023-08-28 15:50:43 -0300

    refatoracao exemplos karloff

--- script.py
+++ script.py
@@ -1,18 +1,12 @@
 #!/usr/bin python3
 
 def main():
-	times = int(input())
-	output = fib(times)
-	print(output)
-
-def fib(times: int) -> int:
-	result = 0
-	past = 1
-	while times > 0:
-		result = result + past
-		past = result
-		times = times - 1
-	return result
+	soma = 0
+	valor = int(input())
+	while not valor == 0:
+		soma = soma + valor
+		valor = int(input())
+	print(soma)
 
 if __name__ == "__main__":
 	main()
